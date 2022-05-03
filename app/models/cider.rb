08b23ac57724f10d0ffc7145789a9d142968cce0
thomas andrieu <than1544@colorado.edu>
2022-05-03 20:51:38 +0200

    change

--- app/models/cider.rb
+++ app/models/cider.rb
@@ -1,11 +1,6 @@
 class Cider < ApplicationRecord
-<<<<<<< HEAD
   has_many :reviews
   belongs_to :reservations
   has_many :reservation
-=======
-  has_one_attached :photo
-  has_many :reviews, :reservations
->>>>>>> master
   validates :name, presence: true
 end
