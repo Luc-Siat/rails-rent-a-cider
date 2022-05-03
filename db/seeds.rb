8744c7138737d7957d57d36bdc38a98f09900394
luc siat <lucsiat@gmail.com>
2022-05-03 20:15:39 +0200

    changed the cider model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cider = Cider.new(name: "Bulmers")
+if cider.save
+  puts "saved"
+else
+  puts "nope"
+end
