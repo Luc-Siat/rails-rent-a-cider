d22a95107d72d4dc9af0e734e6295d193960268d
luc siat <lucsiat@gmail.com>
2022-05-07 15:55:20 +0200

    fixes

--- app/models/cider.rb
+++ app/models/cider.rb
@@ -1,6 +1,6 @@
 class Cider < ApplicationRecord
   has_one_attached :photo
   has_many :reviews
-  has_one :reservation
+  has_one :reservationn, dependent: :destroy
   validates :name, presence: true
 end
